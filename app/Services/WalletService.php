b17ee7cf8ba4bce4736bb3125d7b2b7ecb5c7447
AMK1215 <aungmyokhaing1215@gmail.com>
2025-08-12 09:04:17 +0630

    two d setting update

--- app/Services/WalletService.php
+++ app/Services/WalletService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Enums\TransactionName;
+use App\Enums\DigitTransactionName;
 use App\Enums\TransactionType;
 use App\Models\User;
 use Bavix\Wallet\External\Dto\Extra;
@@ -10,7 +11,7 @@ use Bavix\Wallet\External\Dto\Option;
 
 class WalletService
 {
-    public function forceTransfer(User $from, User $to, float $amount, TransactionName $transaction_name, array $meta = [])
+    public function forceTransfer(User $from, User $to, float $amount, TransactionName|DigitTransactionName $transaction_name, array $meta = [])
     {
         return $from->forceTransferFloat($to, $amount, new Extra(
             deposit: new Option(self::buildTransferMeta($to, $from, $transaction_name, $meta)),
@@ -18,7 +19,7 @@ class WalletService
         ));
     }
 
-    public function transfer(User $from, User $to, float $amount, TransactionName $transaction_name, array $meta = [])
+    public function transfer(User $from, User $to, float $amount, TransactionName|DigitTransactionName $transaction_name, array $meta = [])
     {
         return $from->forceTransferFloat($to, $amount, new Extra(
             deposit: new Option(self::buildTransferMeta($to, $from, $transaction_name, $meta)),
@@ -26,12 +27,12 @@ class WalletService
         ));
     }
 
-    public function deposit(User $user, float $amount, TransactionName $transaction_name, array $meta = [])
+    public function deposit(User $user, float $amount, TransactionName|DigitTransactionName $transaction_name, array $meta = [])
     {
         $user->depositFloat($amount, self::buildDepositMeta($user, $user, $transaction_name, $meta));
     }
 
-    public function withdraw(User $user, float $amount, TransactionName $transaction_name, array $meta = [])
+    public function withdraw(User $user, float $amount, TransactionName|DigitTransactionName $transaction_name, array $meta = [])
     {
         try {
             \Log::info("Attempting to withdraw {$amount} from user {$user->id} for transaction: {$transaction_name->value}");
@@ -53,7 +54,7 @@ class WalletService
         }
     }
 
-    public static function buildTransferMeta(User $user, User $target_user, TransactionName $transaction_name, array $meta = [])
+    public static function buildTransferMeta(User $user, User $target_user, TransactionName|DigitTransactionName $transaction_name, array $meta = [])
     {
         return array_merge([
             'name' => $transaction_name,
@@ -62,7 +63,7 @@ class WalletService
         ], $meta);
     }
 
-    public static function buildDepositMeta(User $user, User $target_user, TransactionName $transaction_name, array $meta = [])
+    public static function buildDepositMeta(User $user, User $target_user, TransactionName|DigitTransactionName $transaction_name, array $meta = [])
     {
         return array_merge([
             'name' => $transaction_name->value,
@@ -71,7 +72,7 @@ class WalletService
         ], $meta);
     }
 
-    public static function buildWithdrawMeta(User $user, User $target_user, TransactionName $transaction_name, array $meta = [])
+    public static function buildWithdrawMeta(User $user, User $target_user, TransactionName|DigitTransactionName $transaction_name, array $meta = [])
     {
         return array_merge([
             'name' => $transaction_name->value,
