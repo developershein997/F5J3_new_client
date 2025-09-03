ba4848e1e6b0e0b936914bd9e1e34905a6727aca
KaungSettThu1873 <kaungsettthu1873@example.com>
2025-09-03 09:47:14 +0630

    WithdrawApiFixingDone

--- app/Http/Controllers/Api/V1/WithDrawRequestController.php
+++ app/Http/Controllers/Api/V1/WithDrawRequestController.php
@@ -2,15 +2,14 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Controllers\Controller;
+use App\Http\Resources\WithdrawResource;
+use App\Models\WithDrawRequest;
+use App\Traits\HttpResponses;
 use Exception;
 use Illuminate\Http\Request;
-use App\Traits\HttpResponses;
-use App\Models\WithDrawRequest;
-use Illuminate\Support\Facades\Log;
-use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use App\Http\Resources\WithdrawResource;
 
 class WithDrawRequestController extends Controller
 {
@@ -25,8 +24,8 @@ class WithDrawRequestController extends Controller
             'payment_type_id' => ['required', 'integer'],
             'password' => ['required']
         ]);
-         Log::info('Withdraw Request Data:', $request->all());
-        // dd($request->all());
+        //  Log::info('Withdraw Request Data:', $request->all());
+
 
         $player = Auth::user();
 
@@ -34,13 +33,10 @@ class WithDrawRequestController extends Controller
             return $this->error('', 'Your password is wrong!', 401);
         }
 
-        // if ($request->amount > $player->balanceFloat) {
-        //     return $this->error('', 'Insufficient Balance!', 401);
-        // }
-
-        if (!isset($player->balanceFloat) || $request->amount > $player->balanceFloat) {
+        if ($request->amount > $player->balanceFloat) {
             return $this->error('', 'Insufficient Balance!', 401);
         }
+
 
         $withdraw = WithDrawRequest::create([
             'user_id' => $player->id,
